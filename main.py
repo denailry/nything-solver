95053ef896eb9adefbaab7006af9f74b2a2f4f06
dionsaputra <dionsaputra666@gmail.com>
2018-09-23 16:25:09 +0700

    Add choice algorithm

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import input_helper
 
 print('file input: ', end="")
 file_name = input()
+print()
 
-input_helper.init_board(file_name)
-input_helper.display_board()
+# input_helper.init_board(file_name)
+# input_helper.display_board()
+
+print('choose algorithm: ')
+print('1. Hill Climbing')
+print('2. Simulated Annealing')
+print('3. Genetic Algorithm')
+print('Your choice: ', end='')
+
+selected_algo = int(input())
+
+if selected_algo in range(1,4):
+    print('Loading...')
+else:
+    print('Algorithm not found')
