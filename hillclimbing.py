be8539b7584a260b4ff8c137918b0ec7b483e0df
daphtya <davidpanjaitan1@gmail.com>
2018-09-23 22:36:30 +0700

    fix hill climbing

--- hillclimbing.py
+++ hillclimbing.py
@@ -1,13 +1,15 @@
 import evaluator as ev #nama placeholder buat modul bersama
 import random
 
+empty_tile = ''
+
 def findPieceScores(board): # the board is a matrix
     # find the scores of each individual piece, sorted from the worst scoring
     pieceScores = []
     for i in range(8):
         for j in range(8):
-            if board[i][j] != '*':
-                score = ev.numConflicting(board, i, j)
+            if board[i][j] != empty_tile:
+                score = ev.numConflicting(board, j, i)
                 score[1] = 64 - score[1]
                 pieceScores.append({'pos':[i,j], 'scr':score})
     pieceScores.sort(key=lambda piece: piece['scr'], reverse=True)
@@ -15,32 +17,36 @@ def findPieceScores(board): # the board is a matrix
 
 def findBestMove(board, toBeMoved_x, toBeMoved_y):
     toBeMoved = board[toBeMoved_x][toBeMoved_y]
-    board[toBeMoved_x][toBeMoved_y] = '*'
+    #print(board)
+    board[toBeMoved_x][toBeMoved_y] = empty_tile
     newScores = []
     bestScore = [64,64]
     bestNewPos = None
     for i in range(8):
         for j in range(8):
-            if board[i][j] == '*':
+            if board[i][j] == empty_tile:
                 board[i][j] = toBeMoved
-                score = ev.numConflicting(board, i, j)
+                score = ev.numConflicting(board, j, i)
                 score[1] = 64 - score[1]
                 newScores.append({'pos':[i,j], 'scr':score})
                 if score < bestScore:
                     bestScore = score
                     bestNewPos = [i,j]
-                board[i][j] = '*'
+                board[i][j] = empty_tile
+    board[toBeMoved_x][toBeMoved_y] = toBeMoved
     
     bestMoves = [x for x in newScores if x['scr'] == bestScore]
     return bestMoves
 
 def movePiece(board, origin, dest):
     board[dest[0]][dest[1]] = board[origin[0]][origin[1]]
-    board[origin[0]][origin[1]] = '*'
+    board[origin[0]][origin[1]] = empty_tile
 
 def hillclimb(board):
+    boredom_threshold = 100
     while True:
         hasmoved = False
+        prevScore = ev.boardNumConflicting(board)
         pieceScores = findPieceScores(board)
         for piece in pieceScores:
             bestMoves = findBestMove(board, piece['pos'][0], piece['pos'][1])
@@ -52,4 +58,9 @@ def hillclimb(board):
 
         if not hasmoved:
             break
-        
+        if ev.boardNumConflicting(board) == prevScore:
+            boredom_threshold -= 1
+            if boredom_threshold == 0:
+                break
+        else:
+            boredom_threshold = 100
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import input_helper
+import evaluator as eva
+import hillclimbing as hc
 
 print('file input: ', end="")
 file_name = input()
@@ -6,6 +8,7 @@ print()
 
 input_helper.init_board(file_name)
 input_helper.display_board(input_helper.board)
+print(eva.boardNumConflicting(input_helper.board))
 
 print()
 print('choose algorithm: ')
@@ -16,7 +19,15 @@ print('Your choice: ', end='')
 
 selected_algo = int(input())
 
+
 if selected_algo in range(1,4):
     print('Loading...')
 else:
     print('Algorithm not found')
+
+if selected_algo == 1:
+    hc.hillclimb(input_helper.board)
+    input_helper.display_board(input_helper.board)
+
+
+print(eva.boardNumConflicting(input_helper.board))
