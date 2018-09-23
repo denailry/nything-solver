20e37feaae253c6155488c8ac9ec93c411548be5
dionsaputra <dionsaputra666@gmail.com>
2018-09-23 16:20:18 +0700

    create input_helper

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-black_piece = []
-white_piece = []
+import input_helper
 
-str_white = "WHITE"
-str_black = "BLACK"
+print('file input: ', end="")
+file_name = input()
 
-def read_file(file_name):
-    data_file = open(file_name, 'r')
-    
-    for row in data_file:
-        arr_row = row.split()
-        if (arr_row[0] == str_white):
-            for i in range(0, int(arr_row[2])):
-                white_piece.append(arr_row[1][0])
-        else:
-            for i in range(0, int(arr_row[2])):
-                print(arr_row[1][0])
-                black_piece.append(arr_row[1][0])
-
-read_file('input1.txt')
-print(black_piece)
-print(white_piece)
+input_helper.init_board(file_name)
+input_helper.display_board()
