4facee834f6b6a0f8633aa81420622df4e23465b
dionsaputra <dionsaputra666@gmail.com>
2018-09-23 16:04:13 +0700

    add read_file in main

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+black_piece = []
+white_piece = []
 
-# put file-reading algorithm here
+str_white = "WHITE"
+str_black = "BLACK"
+
+def read_file(file_name):
+    data_file = open(file_name, 'r')
+    
+    for row in data_file:
+        arr_row = row.split()
+        if (arr_row[0] == str_white):
+            for i in range(0, int(arr_row[2])):
+                white_piece.append(arr_row[1][0])
+        else:
+            for i in range(0, int(arr_row[2])):
+                print(arr_row[1][0])
+                black_piece.append(arr_row[1][0])
+
+read_file('input1.txt')
+print(black_piece)
+print(white_piece)
